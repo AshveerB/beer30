84063a0d7b62405cb5f7943664ebe99ae214bb90
AshveerB <ashveerbhayroo@gmail.com>
2020-10-27 21:01:16 -0700

    add state to search component

--- src/search/Search.js
+++ src/search/Search.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Results from '../results/Results'
 
+
 const Search = () => {
+    const initialState = {
+            searchBar: ''
+    }
+    const [formState, setFormState] = useState(initialState);
+
     const handleSubmit = (event) => {
         event.preventDefault()
-        // <Results />
+        
+        console.log(formState)
+
+        setFormState(initialState);
     }
+    const handleChange = (event) => {
+		setFormState({ ...formState, [event.target.id]: event.target.value });
+	};
     return (
 			<div>
 				Search
                 <form onSubmit={handleSubmit}>
                     <label htmlFor='searchBar'>
-                        <input id='searchBar' />
+                        <input id='searchBar' onChange={handleChange} value={formState.searchBar} />
                         <button type='submit'>Submit</button>
                     </label>
                 </form>
 			</div>
-		);
-};
+        );
+}
 
 export default Search;
