82c63ed3b937c0f4870bedfac44725f4494e594a
AshveerB <ashveerbhayroo@gmail.com>
2020-10-27 21:45:40 -0700

    change route to results

--- src/search/Search.js
+++ src/search/Search.js
@@ -1,35 +1,41 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Results from '../results/Results'
-
+import Results from '../results/Results';
 
 const Search = () => {
-    const initialState = {
-            searchBar: ''
-    }
-    const [formState, setFormState] = useState(initialState);
+	const initialState = {
+		searchBar: '',
+	};
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        
-        console.log(formState)
+	const [formState, setFormState] = useState(initialState);
 
-        setFormState(initialState);
-    }
-    const handleChange = (event) => {
+	const handleSubmit = (event) => {
+		event.preventDefault();
+
+		console.log(formState);
+
+		setFormState(initialState);
+	};
+
+	const handleChange = (event) => {
 		setFormState({ ...formState, [event.target.id]: event.target.value });
 	};
-    return (
-			<div>
-				Search
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor='searchBar'>
-                        <input id='searchBar' onChange={handleChange} value={formState.searchBar} />
-                        <button type='submit'>Submit</button>
-                    </label>
-                </form>
-			</div>
-        );
-}
+
+	return (
+		<div>
+			Search
+			<form onSubmit={handleSubmit}>
+				<label htmlFor='searchBar'>
+					<input
+						id='searchBar'
+						onChange={handleChange}
+						value={formState.searchBar}
+					/>
+					<button type='submit'>Submit</button>
+				</label>
+			</form>
+		</div>
+	);
+};
 
 export default Search;
