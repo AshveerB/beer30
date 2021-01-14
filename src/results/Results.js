68512ca0d5633b1107bcce2b21f0853c2c2833c5
AshveerB <ashveerbhayroo@gmail.com>
2021-01-13 22:06:46 -0800

    media queries for results component

--- src/results/Results.js
+++ src/results/Results.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Results.css'
 
 const Results = ({ bars }) => {
 	return (
 		<div class='results'>
 			{bars.map((bar) => (
 				<Link to={`/details/${bar.id}`} key={bar.id}>
-					<h2 key={bar.id}>{bar.name}</h2>
+					<h2 key={bar.id} className='bar'>{bar.name}</h2>
 				</Link>
 			))}
 		</div>
